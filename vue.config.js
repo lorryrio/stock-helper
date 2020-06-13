7acd4c8832d0ab9b2b4871935ca2c90af8b72b48
罗锐 <luorui@qutoutiao.net>
2020-06-14 00:33:16 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: '/stock-helper/',
+  publicPath: '/',
   indexPath: 'index.html',
   productionSourceMap: true,
   outputDir: path.join(__dirname, './docs'),
