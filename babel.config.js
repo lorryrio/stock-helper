f6b2589fd4667720874c4b7562663f4fbfdff914
罗锐 <luorui@qutoutiao.net>
2020-06-20 16:53:45 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
+const plugins = ['@vue/babel-plugin-transform-vue-jsx'];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console');
+}
+
 module.exports = {
+  plugins,
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
